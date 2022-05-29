9a64a00763ca62fbc5dbca29421424d03c79efd0
test-akun <alvid.ardato@gmail.com>
2022-05-29 05:47:49 +0000

    routing & controller

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -3,16 +3,51 @@ const Transaction = require('../models/Transaction')
 // GET Transaction
 const getTransaction = async (req, res) => {
     try {
-        const transaction = await Transaction.find()
+        const transactions = await Transaction.find()
         if (!transactions) {
             throw new Error('No transactions')
         }
-        res.json(transaction)
+        res.status(200).json(transactions)
     } catch (error) {
         res.status(400).json({ message: error.message })
     }
 }
 
+const postTransaction = async (req, res) => {
+    // const { value, date } = req.body
+    const newTransaction = new Transaction(req.body)
+
+    try {
+        const transaction = await newTransaction.save()
+        if (!transaction) {
+            throw new Error('There was an error saving the transaction')
+        }
+        res.status(200).json(transaction)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+const deleteTransaction = async (req, res) => {
+    const { id } = req.params
+    try {
+        const transaction = await Transaction.findById(id)
+        if (!transaction) {
+            throw new Error('No transaction was found')
+        }
+        const removed = await transaction.remove()
+
+        if (!removed) {
+            throw new Error('There was a problem deleting the transaction')
+        }
+        res.status(200).json({ id })
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
 module.exports = {
     getTransaction,
+    postTransaction,
+    deleteTransaction
 }
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
-const { getTransaction } = require('../controllers/transactions')
+const { getTransaction, postTransaction, deleteTransaction } = require('../controllers/transactions')
 
 const router = Router()
 
 router.get('/', getTransaction)
+router.post('/', postTransaction)
+router.delete('/:id', deleteTransaction)
 
 module.exports = router
