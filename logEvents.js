8458663914cc2df8cbddafd254d443135907b1d4
test-akun <alvid.ardato@gmail.com>
2022-06-01 05:02:15 +0000

    create server

--- logEvents.js
+++ logEvents.js
@@ -7,14 +7,14 @@ const fsPromise = require('fs/promises')
 const fs = require('fs')
 const path = require('path')
 
-const logEvents = async (message) => {
+const logEvents = async (message, logName) => {
     const dateTime = format(new Date(), 'yyyyMMdd\tHH:mm:ss')
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`
     try {
         if (!fs.existsSync(path.join(__dirname, 'logs'))) {
             await fsPromise.mkdir(path.join(__dirname, 'logs'))
         }
-        await fsPromise.appendFile(path.join(__dirname, 'logs', 'eventlog.txt'), logItem)
+        await fsPromise.appendFile(path.join(__dirname, 'logs', logName), logItem)
     } catch (error) {
         console.error(error)
     }
--- server.js
+++ server.js
@@ -1,13 +1,56 @@
-const EventEmitter = require('events');
-const { setTimeout } = require('timers/promises');
-const logEvents = require('./logEvents');
-class Emitter extends EventEmitter { }
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
 
-// initialize object
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter { };
+// initialize object 
 const myEmitter = new Emitter();
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
+
+    const extension = path.extname(req.url);
+
+    let contentType;
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+
+    res.writeHead(301, { 'Location': '/' });
+    res.end();
+})
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 // add listener for the log event
-myEmitter.on('log', msg => logEvents(msg))
+// myEmitter.on('log', msg => logEvents(msg))
 
 // setTimeout(() => myEmitter.emit('gol', 'log event emitted'), 2000)
-setTimeout(2000, myEmitter.emit('log', 'log event emiited!'))
+// setTimeout(2000, myEmitter.emit('log', 'log event emiited!'))
